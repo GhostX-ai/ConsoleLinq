9cc5a82e70c0a36307a26582fae2e7e104ee9c9b
GhostX <egamovkhurshed29@gmail.com>
2020-05-11 22:28:09 +0500

    First Task has been done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 21654984;
+            char[] snum = num.ToString().ToCharArray();
+            foreach (var x in snum.Reverse())
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
